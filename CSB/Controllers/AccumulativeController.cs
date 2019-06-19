5a7048f43491edf08204e396b3ba0549caf4348b
Al Saleh <alayham@alayham.com>
2019-06-19 21:52:23 +0200

    Added an accumulative view of the data

---
+++ CSB/Controllers/AccumulativeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CSB.Models.ViewModels;
+
+namespace CSB.Controllers
+{
+    public class AccumulativeController : Controller
+    {
+        // GET: Accomulative
+        public ActionResult Index()
+        {
+            return View(new MapViewModel());
+        }
+    }
+}
